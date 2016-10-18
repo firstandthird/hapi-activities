e7ac6e0167e17051ee9655e63df13e2b3eb39e16
Chris <orthagonal1@gmail.com>
2016-10-18 12:20:01 -0500

    updated working version

--- index.js
+++ index.js
@@ -2,17 +2,18 @@ const mongo = require('mongodb');
 const async = require('async');
 const _ = require('lodash');
 
-const defaults = {
-  mongo: {
-    host: 'mongodb://localhost:27017',
-    collectionName: 'hapi-activities'
-  },
-  interval: 5 * 60 * 1000, // 5 minutes
-  log: true
-};
-
 exports.register = (server, options, next) => {
+  const defaults = {
+    mongo: {
+      host: 'mongodb://localhost:27017',
+      collectionName: 'hapi-activities'
+    },
+    timeout: 30 * 1000, // max time an action can take, default is 30 secs, set to false for infinity
+    interval: 5 * 60 * 1000, // 5 minutes
+    log: true
+  };
   const settings = _.defaults(options, defaults);
+  // connect to db:
   mongo.connect(settings.mongo.host, (err, db) => {
     if (err) {
       return next(err);
@@ -20,47 +21,68 @@ exports.register = (server, options, next) => {
     // initialize the server object:
     const collection = db.collection(settings.mongo.collectionName);
 
-    // update activities based on a specific server activityName:
-    const updateActivities = (activityName, allDone) => {
+    // update all activities:
+    const updateActivities = (allDone) => {
+      // automap this for clarity:
+
       async.auto({
         // 1. fetch all open activities from db:
         collection: (done) => {
           collection
-          .find({ status: 'waiting', activityName })
+          .find({ status: 'waiting' })
           .toArray(done);
         },
         // 2. mark them as underway:
         markProcessing: ['collection', (results, saveDone) => {
-          async.each(results.collection, (activity, done) => {
+          if (!results.collection || results.collection.length === 0) {
+            return saveDone();
+          }
+          const ids = [];
+          results.collection.forEach((item) => {
             if (settings.log) {
-              server.log(['hapi-activities', 'starting-activity', 'debug'], activity);
+              server.log(['hapi-activities', 'starting-activity', 'debug'], { message: 'Processing underway for item', data: item });
             }
-            activity.status = 'processing';
-            collection.update({ _id: activity._id }, activity, done);
-          }, saveDone);
+            ids.push(item._id);
+          });
+          collection.update({ _id: { $in: ids } }, { $set: { status: 'processing' } }, { multi: true }, saveDone);
         }],
-        // 3. process them:
+        // 3. for each activity, launch its actions in parallel and let each action's callback handler report back:
         process: ['markProcessing', (results, done) => {
           async.eachSeries(results.collection, (activity, seriesDone) => {
-            // execute all actions in parallel:
-            if (settings.log) {
-              server.log(['hapi-activities', 'executing-activity', 'debug'], activity);
-            }
+            const updatedActivity = {
+              results: []
+            };
+            // will launch actions in parallel:
             async.each(settings.activities[activity.activityName], (action, eachDone) => {
-              eachDone(server.methods[action](activity.activityData));
-            }, seriesDone);
-          }, done);
+              if (settings.log) {
+                server.log(['hapi-activities', 'executing-action', 'debug'], { message: `Executing activity ${activity.activityName} action ${action}`, data: activity.activityData });
+              }
+              // if a timeout is specified then put a timeout wrapper around the server method call:
+              const actionCall = settings.timeout ? async.timeout(server.methods[action], settings.timeout) : server.methods[action];
+              actionCall(activity.activityData, (error, output) => {
+                if (settings.log) {
+                  server.log(['hapi-activities', 'finished-action', 'debug'], { message: `Finished executing activity ${activity._id} action ${action}`, data: output });
+                }
+                // will handle async's ETIMEDOUT error as well as other errors:
+                if (error) {
+                  updatedActivity.results.push({ action, error });
+                  updatedActivity.status = 'failed';
+                } else {
+                  updatedActivity.results.push({ action, output });
+                }
+                eachDone();
+              });
+            }, () => {
+              // once all actions are in, we can update the whole activity:
+              updatedActivity.status = (updatedActivity.status === 'failed') ? 'failed' : 'complete';
+              updatedActivity.completedOn = new Date();
+              collection.update({ _id: activity._id }, { $set: updatedActivity }, (err) => {
+                seriesDone();
+              });
+            });
+          });
+          return done();
         }],
-        // 4. mark them as complete:
-        markComplete: ['process', (results, saveDone) => {
-          async.each(results.collection, (activity, done) => {
-            if (settings.log) {
-              server.log(['hapi-activities', 'finished-activity', 'debug'], activity);
-            }
-            activity.status = 'complete';
-            collection.update({ _id: activity._id }, activity, done);
-          }, saveDone);
-        }]
       }, (autoErr) => {
         if (autoErr) {
           server.log(autoErr);
@@ -72,7 +94,7 @@ exports.register = (server, options, next) => {
     // register the 'activity' method with the server:
     server.method('activity', (activityName, activityData) => {
       if (settings.log) {
-        server.log(['hapi-activities', 'new-activity', 'debug'], activityData);
+        server.log(['hapi-activities', 'new-activity', 'debug'], { message: `Registering a new activity: '${activityName}'`, data: activityData });
       }
       collection.insertOne({
         activityName,
@@ -88,12 +110,7 @@ exports.register = (server, options, next) => {
 
     // manage the interval polling:
     const timer = () => {
-      async.each(Object.keys(settings.activities), (activityName, done) => {
-        updateActivities(activityName, done);
-      }, (timerErr) => {
-        if (timerErr) {
-          server.log(timerErr);
-        }
+      updateActivities(() => {
         setTimeout(timer, settings.interval);
       });
     };
--- test/test.activities.js
+++ test/test.activities.js
@@ -3,18 +3,29 @@ const code = require('code');   // assertion library
 const Lab = require('lab');
 const lab = exports.lab = Lab.script();
 const Hapi = require('hapi');
-const hapiActivities = require('../');
+const mongo = require('mongodb');
 
-lab.experiment('hapi-auto-handler', () => {
+lab.experiment('hapi-activities', () => {
   let server;
+  let db;
+  let collection;
+  let hapiActivities;
   lab.beforeEach((done) => {
+    hapiActivities = require('../');
     server = new Hapi.Server({
       debug: {
-        log: ['hapi-activities']
+        log: ['hapi-activities', 'error']
       }
     });
     server.connection({ port: 8080 });
-    done();
+    mongo.connect('mongodb://localhost:27017', (err, theDb) => {
+      if (err) {
+        console.log(err)
+      }
+      db = theDb;
+      collection = db.collection('hapi-activities-test');
+      collection.drop(done);
+    });
   });
 
   lab.afterEach((done) => {
@@ -22,25 +33,31 @@ lab.experiment('hapi-auto-handler', () => {
       done();
     });
   });
-
-  lab.test('lets you set up a list of activities', { timeout: 10000 }, (done) => {
+  lab.test('adds a server method that will process an activity composed of actions', { timeout: 10000 }, (done) => {
     const numberOfCalls = {
       kickball: 0,
       trumpet: 0,
       pottery: 0
     };
-    server.method('kickball', () => {
+    server.method('kickball', (data, callback) => {
       numberOfCalls.kickball ++;
+      callback(null, numberOfCalls.kickball);
     });
-    server.method('trumpet', (data) => {
+    server.method('trumpet', (data, callback) => {
       numberOfCalls.trumpet = data.age;
+      callback(null, numberOfCalls.trumpet);
     });
-    server.method('pottery', () => {
+    server.method('pottery', (data, callback) => {
       numberOfCalls.pottery ++;
+      callback(null, numberOfCalls.pottery);
     });
     server.register({
       register: hapiActivities,
       options: {
+        mongo: {
+          host: 'mongodb://localhost:27017',
+          collectionName: 'hapi-activities-test'
+        },
         interval: 1000, // 1 second
         activities: {
           'after school': [
@@ -62,8 +79,63 @@ lab.experiment('hapi-auto-handler', () => {
         code.expect(numberOfCalls.kickball).to.equal(1);
         code.expect(numberOfCalls.trumpet).to.equal(7);
         code.expect(numberOfCalls.pottery).to.equal(1);
-        done();
-      }, 5000);
+        collection.findOne({}, (err, activity) => {
+          code.expect(activity.status).to.equal('complete');
+          code.expect(activity.results.length).to.equal(3);
+          server.methods.activity('after school', {
+            name: 'sven',
+            age: 5
+          });
+          setTimeout(() => {
+            code.expect(numberOfCalls.kickball).to.equal(2);
+            code.expect(numberOfCalls.trumpet).to.equal(5);
+            code.expect(numberOfCalls.pottery).to.equal(2);
+            collection.findOne({}, (err, activity) => {
+              code.expect(activity.status).to.equal('complete');
+              code.expect(activity.results.length).to.equal(3);
+              done();
+            });
+          }, 2500);
+        });
+      }, 2500);
+    });
+  });
+
+  lab.test('can handle and report callback errors during an action', { timeout: 8000 }, (done) => {
+    const numberOfCalls = {
+      breakfast: 0
+    };
+    server.method('breakfast', (data, callback) => {
+      numberOfCalls.breakfast ++;
+      return callback('I am an error');
+    });
+    server.register({
+      register: hapiActivities,
+      options: {
+        mongo: {
+          host: 'mongodb://localhost:27017',
+          collectionName: 'hapi-activities-test'
+        },
+        interval: 500,
+        activities: {
+          'before school': ['breakfast']
+        }
+      }
+    }, (err) => {
+      server.methods.activity('before school', {
+        name: 'sven',
+        age: 5
+      });
+      setTimeout(() => {
+        code.expect(numberOfCalls.breakfast).to.equal(1);
+        // check the db object:
+        collection.findOne({}, (err, activity) => {
+          code.expect(activity.status).to.equal('failed');
+          code.expect(activity.results.length).to.equal(1);
+          code.expect(activity.results[0].error).to.equal('I am an error');
+          done();
+        });
+      }, 3000);
     });
   });
 });
