bdb7fae9dea85289696e7ac928409c2dd7fa9e24
Chris <orthagonal1@gmail.com>
2017-03-02 16:45:58 -0600

    convert to tape

--- test/test.hooks.js
+++ test/test.hooks.js
@@ -28,17 +28,89 @@ const test = tapeExtras(tape, {
       });
     });
   },
-  // afterEach(done) {
-    // console.log('and this is the end')
-    // console.log(server)
-    // console.log(db)
-    // server.stop(done);
-  // }
+  after(params, done) {
+    params[2].drop();
+    done();
+  }
 });
 
 test('adds a server method that will process an hook composed of actions', (t, fixtures) => {
   const server = fixtures[0];
-  const db = fixtures[1];
+  const collection = fixtures[2];
+  const hapiHooks = fixtures[3];
+  const numberOfCalls = {
+    kickball: 0,
+    trumpet: 0,
+    pottery: 0
+  };
+  server.method('kickball', (data, callback) => {
+    console.log('this is the kickball')
+    console.log(numberOfCalls.kickball)
+    numberOfCalls.kickball ++;
+    callback(null, numberOfCalls.kickball);
+  });
+  server.method('trumpet', (data, callback) => {
+    numberOfCalls.trumpet = data.age;
+    callback(null, numberOfCalls.trumpet);
+  });
+  server.method('pottery', (data, callback) => {
+    numberOfCalls.pottery ++;
+    callback(null, numberOfCalls.pottery);
+  });
+  server.register({
+    register: hapiHooks,
+    options: {
+      mongo: {
+        host: 'mongodb://localhost:27017',
+        collectionName: 'hapi-hooks-test'
+      },
+      interval: 1000, // 1 second
+      hooks: {
+        'after school': [
+          'kickball',
+          'trumpet',
+          'pottery',
+        ]
+      }
+    }
+  }, (err) => {
+    if (err) {
+      console.log(err);
+    }
+    server.methods.hook('after school', {
+      name: 'bob',
+      age: 7
+    });
+    setTimeout(() => {
+      t.equal(numberOfCalls.kickball, 1);
+      t.equal(numberOfCalls.trumpet, 7);
+      t.equal(numberOfCalls.pottery, 1);
+      collection.findOne({}, (err, hook) => {
+        t.equal(hook.status, 'complete');
+        t.equal(hook.results.length, 3);
+        server.methods.hook('after school', {
+          name: 'sven',
+          age: 5
+        });
+        setTimeout(() => {
+          t.equal(numberOfCalls.kickball, 2);
+          t.equal(numberOfCalls.trumpet, 5);
+          t.equal(numberOfCalls.pottery, 2);
+          collection.findOne({}, (err, hook2) => {
+            t.equal(hook2.status, 'complete');
+            t.equal(hook2.results.length, 3);
+            collection.drop();
+            t.end();
+          });
+        }, 2500);
+      });
+    }, 2500);
+  });
+});
+/*
+
+test('adds a server method that will process an hook composed of actions', (t, fixtures) => {
+  const server = fixtures[0];
   const collection = fixtures[2];
   const hapiHooks = fixtures[3];
   const numberOfCalls = {
@@ -107,304 +179,262 @@ test('adds a server method that will process an hook composed of actions', (t, f
     }, 2500);
   });
 });
-/*
-lab.experiment('hapi-hooks', () => {
 
-  lab.test('adds a server method that will process an hook composed of actions', { timeout: 10000 }, (done) => {
-    const numberOfCalls = {
-      kickball: 0,
-      trumpet: 0,
-      pottery: 0
-    };
-    server.method('kickball', (data, callback) => {
-      numberOfCalls.kickball ++;
-      callback(null, numberOfCalls.kickball);
-    });
-    server.method('trumpet', (data, callback) => {
-      numberOfCalls.trumpet = data.age;
-      callback(null, numberOfCalls.trumpet);
-    });
-    server.method('pottery', (data, callback) => {
-      numberOfCalls.pottery ++;
-      callback(null, numberOfCalls.pottery);
-    });
-    server.register({
-      register: hapiHooks,
-      options: {
-        mongo: {
-          host: 'mongodb://localhost:27017',
-          collectionName: 'hapi-hooks-test'
-        },
-        interval: 1000, // 1 second
-        hooks: {
-          'after school': [
-            'kickball',
-            'trumpet',
-            'pottery',
-          ]
-        }
+test('supports foo.bar for methods', (t, fixtures) => {
+  const server = fixtures[0];
+  const db = fixtures[1];
+  const collection = fixtures[2];
+  const hapiHooks = fixtures[3];
+  let numberOfCalls = 0;
+  server.method('foo.bar', (data, callback) => {
+    numberOfCalls ++;
+    callback();
+  });
+  server.register({
+    register: hapiHooks,
+    options: {
+      mongo: {
+        host: 'mongodb://localhost:27017',
+        collectionName: 'hapi-hooks-test'
+      },
+      interval: 100, // 1 second
+      hooks: {
+        'after school': ['foo.bar']
       }
-    }, (err) => {
-      if (err) {
-        console.log(err);
+    }
+  }, () => {
+    server.methods.hook('after school', {
+      name: 'bob',
+      age: 7
+    });
+    setTimeout(() => {
+      t.equal(numberOfCalls, 1);
+      t.end();
+    }, 2500);
+  });
+});
+
+test('"decorate" option will register the method with "server.decorate" instead of "server.method"', (t, fixtures) => {
+  const server = fixtures[0];
+  const db = fixtures[1];
+  const collection = fixtures[2];
+  const hapiHooks = fixtures[3];
+
+  const numberOfCalls = {
+    kickball: 0,
+    trumpet: 0,
+    pottery: 0
+  };
+  server.method('kickball', (data, callback) => {
+    numberOfCalls.kickball ++;
+    callback(null, numberOfCalls.kickball);
+  });
+  server.method('trumpet', (data, callback) => {
+    numberOfCalls.trumpet = data.age;
+    callback(null, numberOfCalls.trumpet);
+  });
+  server.method('pottery', (data, callback) => {
+    numberOfCalls.pottery ++;
+    callback(null, numberOfCalls.pottery);
+  });
+  server.register({
+    register: hapiHooks,
+    options: {
+      decorate: true,
+      mongo: {
+        host: 'mongodb://localhost:27017',
+        collectionName: 'hapi-hooks-test'
+      },
+      interval: 1000, // 1 second
+      hooks: {
+        'after school': [
+          'kickball',
+          'trumpet',
+          'pottery',
+        ]
       }
-      server.methods.hook('after school', {
-        name: 'bob',
-        age: 7
+    }
+  }, (err) => {
+    if (err) {
+      throw err;
+    }
+    server.hook('after school', {
+      name: 'bob',
+      age: 7
+    });
+    setTimeout(() => {
+      t.equal(numberOfCalls.kickball, 1);
+      t.equal(numberOfCalls.trumpet, 7);
+      t.equal(numberOfCalls.pottery, 1);
+      t.end();
+    }, 2500);
+  });
+});
+
+test('can handle and report callback errors during an action', (t, fixtures) => {
+  const server = fixtures[0];
+  const db = fixtures[1];
+  const collection = fixtures[2];
+  const hapiHooks = fixtures[3];
+  const numberOfCalls = {
+    breakfast: 0
+  };
+  server.method('breakfast', (data, callback) => {
+    numberOfCalls.breakfast ++;
+    return callback('I am an error');
+  });
+  server.register({
+    register: hapiHooks,
+    options: {
+      mongo: {
+        host: 'mongodb://localhost:27017',
+        collectionName: 'hapi-hooks-test'
+      },
+      interval: 500,
+      hooks: {
+        'before school': ['breakfast']
+      }
+    }
+  }, (err) => {
+    if (err) {
+      throw err;
+    }
+    server.methods.hook('before school', {
+      name: 'sven',
+      age: 5
+    });
+    setTimeout(() => {
+      t.equal(numberOfCalls.breakfast, 1);
+      // check the db object:
+      collection.findOne({}, (err2, hook) => {
+        t.equal(hook.status, 'failed');
+        t.equal(hook.results.length, 1);
+        t.equal(hook.results[0].error, 'I am an error');
+        t.end();
       });
-      setTimeout(() => {
-        code.expect(numberOfCalls.kickball).to.equal(1);
-        code.expect(numberOfCalls.trumpet).to.equal(7);
-        code.expect(numberOfCalls.pottery).to.equal(1);
-        collection.findOne({}, (err, hook) => {
-          code.expect(hook.status).to.equal('complete');
-          code.expect(hook.results.length).to.equal(3);
-          server.methods.hook('after school', {
-            name: 'sven',
-            age: 5
-          });
-          setTimeout(() => {
-            code.expect(numberOfCalls.kickball).to.equal(2);
-            code.expect(numberOfCalls.trumpet).to.equal(5);
-            code.expect(numberOfCalls.pottery).to.equal(2);
-            collection.findOne({}, (err, hook2) => {
-              code.expect(hook2.status).to.equal('complete');
-              code.expect(hook2.results.length).to.equal(3);
-              done();
-            });
-          }, 2500);
-        });
-      }, 2500);
-    });
+    }, 3000);
   });
+});
 
-  lab.test('supports foo.bar for methods', { timeout: 5000 }, (done) => {
-    let numberOfCalls = 0;
-    server.method('foo.bar', (data, callback) => {
-      numberOfCalls ++;
-      callback();
-    });
-    server.register({
-      register: hapiHooks,
-      options: {
-        mongo: {
-          host: 'mongodb://localhost:27017',
-          collectionName: 'hapi-hooks-test'
-        },
-        interval: 100, // 1 second
-        hooks: {
-          'after school': ['foo.bar']
-        }
+test('can handle and report server errors during an action', (t, fixtures) => {
+  const server = fixtures[0];
+  const db = fixtures[1];
+  const collection = fixtures[2];
+  const hapiHooks = fixtures[3];
+
+  const numberOfCalls = {
+    breakfast: 0
+  };
+  server.method('breakfast', (data, callback) => {
+    numberOfCalls.breakfast ++;
+    return not.a.thing();
+  });
+  server.register({
+    register: hapiHooks,
+    options: {
+      mongo: {
+        host: 'mongodb://localhost:27017',
+        collectionName: 'hapi-hooks-test'
+      },
+      interval: 500,
+      hooks: {
+        'during school': ['breakfast']
       }
-    }, () => {
-      server.methods.hook('after school', {
-        name: 'bob',
-        age: 7
+    }
+  }, (err) => {
+    if (err) {
+      throw err;
+    }
+    server.methods.hook('during school', {
+      name: 'sven',
+      age: 5
+    });
+    setTimeout(() => {
+      t.equal(numberOfCalls.breakfast, 1);
+      // check the db object:
+      collection.findOne({ hookName: 'during school' }, (err2, hook) => {
+        if (err2) {
+          throw err2;
+        }
+        t.equal(hook.status, 'failed');
+        t.equal(hook.results.length, 1);
+        t.equal(hook.results[0].error).to.include('not is not defined');
+        t.end();
       });
+    }, 3000);
+  });
+});
+
+test('handles actions passed in a { method s: <method>, data: <data> } form', (t, fixtures) => {
+  const server = fixtures[0];
+  const db = fixtures[1];
+  const collection = fixtures[2];
+  const hapiHooks = fixtures[3];
+  let passedData = null;
+  server.method('airplanes', (data, callback) => {
+    console.log('^')
+    console.log('^')
+    console.log('^')
+    passedData = data;
+    callback(null, passedData);
+  });
+  server.register({
+    register: hapiHooks,
+    options: {
+      mongo: {
+        host: 'mongodb://localhost:27017',
+        collectionName: 'hapi-hooks-test'
+      },
+      interval: 1000, // 1 second
+      hooks: {
+        models: [{
+          method: 'airplanes',
+          data: { data1: 'is data 1' }
+        }]
+      }
+    }
+  }, (err) => {
+    if (err) {
+      throw err;
+    }
+    server.methods.hook('models', { data2: 'is data 2' });
+    setTimeout(() => {
+      t.equal(passedData.data1, 'is data 1');
+      t.equal(passedData.data2, 'is data 2');
+      // you can still over-ride the defaults:
+      passedData = null;
+      server.methods.hook('models', { data1: 'is data 2' });
       setTimeout(() => {
-        code.expect(numberOfCalls).to.equal(1);
-        done();
+        t.equal(passedData.data1, 'is data 2');
+        t.end();
       }, 2500);
-    });
+    }, 2500);
   });
+});
 
-  lab.test('"decorate" option will register the method with "server.decorate" instead of "server.method"', { timeout: 7000 }, (done) => {
-    const numberOfCalls = {
-      kickball: 0,
-      trumpet: 0,
-      pottery: 0
-    };
-    server.method('kickball', (data, callback) => {
-      numberOfCalls.kickball ++;
-      callback(null, numberOfCalls.kickball);
-    });
-    server.method('trumpet', (data, callback) => {
-      numberOfCalls.trumpet = data.age;
-      callback(null, numberOfCalls.trumpet);
-    });
-    server.method('pottery', (data, callback) => {
-      numberOfCalls.pottery ++;
-      callback(null, numberOfCalls.pottery);
-    });
-    server.register({
-      register: hapiHooks,
-      options: {
-        decorate: true,
-        mongo: {
-          host: 'mongodb://localhost:27017',
-          collectionName: 'hapi-hooks-test'
-        },
-        interval: 1000, // 1 second
-        hooks: {
-          'after school': [
-            'kickball',
-            'trumpet',
-            'pottery',
-          ]
-        }
-      }
-    }, (err) => {
-      if (err) {
-        throw err;
-      }
-      server.hook('after school', {
-        name: 'bob',
-        age: 7
-      });
-      setTimeout(() => {
-        code.expect(numberOfCalls.kickball).to.equal(1);
-        code.expect(numberOfCalls.trumpet).to.equal(7);
-        code.expect(numberOfCalls.pottery).to.equal(1);
-        done();
-      }, 2500);
-    });
-  });
-
-  lab.test('can handle and report callback errors during an action', { timeout: 8000 }, (done) => {
-    const numberOfCalls = {
-      breakfast: 0
-    };
-    server.method('breakfast', (data, callback) => {
-      numberOfCalls.breakfast ++;
-      return callback('I am an error');
-    });
-    server.register({
-      register: hapiHooks,
-      options: {
-        mongo: {
-          host: 'mongodb://localhost:27017',
-          collectionName: 'hapi-hooks-test'
-        },
-        interval: 500,
-        hooks: {
-          'before school': ['breakfast']
-        }
-      }
-    }, (err) => {
-      if (err) {
-        throw err;
-      }
-      server.methods.hook('before school', {
-        name: 'sven',
-        age: 5
-      });
-      setTimeout(() => {
-        code.expect(numberOfCalls.breakfast).to.equal(1);
-        // check the db object:
-        collection.findOne({}, (err2, hook) => {
-          code.expect(hook.status).to.equal('failed');
-          code.expect(hook.results.length).to.equal(1);
-          code.expect(hook.results[0].error).to.equal('I am an error');
-          done();
-        });
-      }, 3000);
-    });
-  });
-
-  lab.test('can handle and report server errors during an action', { timeout: 8000 }, (done) => {
-    const numberOfCalls = {
-      breakfast: 0
-    };
-    server.method('breakfast', (data, callback) => {
-      numberOfCalls.breakfast ++;
-      return not.a.thing();
-    });
-    server.register({
-      register: hapiHooks,
-      options: {
-        mongo: {
-          host: 'mongodb://localhost:27017',
-          collectionName: 'hapi-hooks-test'
-        },
-        interval: 500,
-        hooks: {
-          'during school': ['breakfast']
-        }
-      }
-    }, (err) => {
-      if (err) {
-        throw err;
-      }
-      server.methods.hook('during school', {
-        name: 'sven',
-        age: 5
-      });
-      setTimeout(() => {
-        code.expect(numberOfCalls.breakfast).to.equal(1);
-        // check the db object:
-        collection.findOne({ hookName: 'during school' }, (err2, hook) => {
-          if (err2) {
-            throw err2;
-          }
-          code.expect(hook.status).to.equal('failed');
-          code.expect(hook.results.length).to.equal(1);
-          code.expect(hook.results[0].error).to.include('not is not defined');
-          done();
-        });
-      }, 3000);
-    });
-  });
-
-  lab.test('handles actions passed in a { method s: <method>, data: <data> } form', { timeout: 10000 }, (done) => {
-    let passedData = null;
-    server.method('airplanes', (data, callback) => {
-      passedData = data;
-      callback(null, passedData);
-    });
-    server.register({
-      register: hapiHooks,
-      options: {
-        mongo: {
-          host: 'mongodb://localhost:27017',
-          collectionName: 'hapi-hooks-test'
-        },
-        interval: 1000, // 1 second
-        hooks: {
-          models: [{
-            method: 'airplanes',
-            data: { data1: 'is data 1' }
-          }]
-        }
-      }
-    }, (err) => {
-      if (err) {
-        throw err;
-      }
-      server.methods.hook('models', { data2: 'is data 2' });
-      setTimeout(() => {
-        code.expect(passedData.data1).to.equal('is data 1');
-        code.expect(passedData.data2).to.equal('is data 2');
-        // you can still over-ride the defaults:
-        passedData = null;
-        server.methods.hook('models', { data1: 'is data 2' });
-        setTimeout(() => {
-          code.expect(passedData.data1).to.equal('is data 2');
-          done();
-        }, 2500);
-      }, 2500);
-    });
-  });
-
-  lab.test('will not add an hook if it does not exist', { timeout: 10000 }, (done) => {
-    server.register({
-      register: hapiHooks,
-      options: {
-        mongo: {
-          host: 'mongodb://localhost:27017',
-          collectionName: 'hapi-hooks-test'
-        },
-        interval: 1000, // 1 second
-        hooks: {} // no hooks
-      }
-    }, (err) => {
-      if (err) {
-        throw err;
-      }
-      server.methods.hook('perpetual motion', {});
-      setTimeout(() => {
-        done();
-      }, 2500);
-    });
+test('will not add an hook if it does not exist', (t, fixtures) => {
+  const server = fixtures[0];
+  const db = fixtures[1];
+  const collection = fixtures[2];
+  const hapiHooks = fixtures[3];
+  server.register({
+    register: hapiHooks,
+    options: {
+      mongo: {
+        host: 'mongodb://localhost:27017',
+        collectionName: 'hapi-hooks-test'
+      },
+      interval: 1000, // 1 second
+      hooks: {} // no hooks
+    }
+  }, (err) => {
+    if (err) {
+      throw err;
+    }
+    server.methods.hook('perpetual motion', {});
+    setTimeout(() => {
+      t.end();
+    }, 2500);
   });
 });
 */
